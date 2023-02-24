679e9101eb8b0c60110817d732d52eeeedff5a38
Alin Radut <alin.claudiu.radut@gmail.com>
2023-02-24 16:58:56 +0200

    Add relevant header files to owncloud.h

--- OCCommunicationLib/ownCloud/ownCloud.h
+++ OCCommunicationLib/ownCloud/ownCloud.h
@@ -17,3 +17,19 @@ FOUNDATION_EXPORT const unsigned char ownCloudVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <ownCloud/PublicHeader.h>
 
 
+#import <ownCloud/OCTrustedCertificatesStore.h>
+#import <ownCloud/OCOAuth2Configuration.h>
+#import <ownCloud/OCShareUser.h>
+#import <ownCloud/OCWebDAVClient.h>
+#import <ownCloud/OCServerFeatures.h>
+#import <ownCloud/OCErrorMsg.h>
+#import <ownCloud/OCFrameworkConstants.h>
+#import <ownCloud/OCFileDto.h>
+#import <ownCloud/OCCredentialsDto.h>
+#import <ownCloud/OCChunkDto.h>
+#import <ownCloud/OCOAuth2Manager.h>
+#import <ownCloud/OCCommunication.h>
+#import <ownCloud/OCSharedDto.h>
+#import <ownCloud/OCCredentialsStorage.h>
+#import <ownCloud/OCCapabilities.h>
+#import <ownCloud/OCConstants.h>
